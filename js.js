c03f65ac11ea6030cd449d179888302f411ef69f
Binazir <104298006+Binazirfarokhi@users.noreply.github.com>
2023-02-28 17:50:43 -0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("exit");
+let element = document.getElementById("toggle");
+
+button.addEventListener("click", function(){
+
+
+    element.classList.toggle("wrapper-display");
+    
+});
