8ac767cf56f8a8c9d132bf454e11b98a2ef67db4
azim763 <azim763@yahoo.com>
2023-03-16 12:19:09 -0700

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById("exit");
-let element = document.getElementById("toggle");
-
-button.addEventListener("click", function(){
-
-
-    element.classList.toggle("wrapper-display");
-    
-});
